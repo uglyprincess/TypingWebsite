2588d153678493059783e605db942b127b6565c5
uglyprincess <the.boy.who.lived.3x4@gmail.com>
2021-10-16 02:16:01 +0530

    Converted Each Word into Individual Component

--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -1,14 +1,17 @@
 import react from 'react';
+import './word.css';
 
 export default function Word(props) {
 
     if(props.status === true)
-        return 0;
+        return <div className="current">
+            {props.value}
+        </div>;
 
     if(props.status === false)
     {
-        return (<div>
-            {props.value + ' '}
+        return (<div className="box">
+            {props.value}
         </div>);
     }
 }
--- src/components/WordWindow.jsx
+++ src/components/WordWindow.jsx
@@ -1,5 +1,6 @@
 import {React} from 'react';
 import Word from './Word.jsx';
+import './word.css';
 
 //You will receive a single word. Highlight it accordingly. 
 
@@ -7,8 +8,13 @@ export default function Words(props) {
 
     console.log(props.wordArray);
 
-    var arr = props.wordArray.slice(0, 50);
+    var curr = props.wordArray[0];
 
-    return arr.map((word) => word + ' ');
+    var arr = props.wordArray.slice(1, 50);
+
+    return <div className='parent'>
+        <Word status={true} value={curr}/>
+        {arr.map((word) => <Word status={false} value={word}/>)}
+    </div>;
 
 }
